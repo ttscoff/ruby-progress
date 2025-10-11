aa7383a0337cc3f417de55ea45d2cfe17740e64f
Brett Terpstra <me@brettterpstra.com>
2025-10-11 07:47:29 -0500

    add fill shim

--- lib/ruby-progress/version.rb
+++ lib/ruby-progress/version.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module RubyProgress
-  VERSION = '1.2.2'
+  # Main gem version
+  VERSION = '1.2.3'
+
+  # Component-specific versions
   WORM_VERSION = '1.1.2'
   TWIRL_VERSION = '1.1.2'
   RIPPLE_VERSION = '1.1.2'
+  FILL_VERSION = '1.0.1'
 end
